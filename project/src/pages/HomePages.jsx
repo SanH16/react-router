7eb1b15498ea422e6f6c1c5face3a46dbd8d4d4d
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-10-26 23:24:12 +0700

    update

--- project/src/pages/HomePages.jsx
+++ project/src/pages/HomePages.jsx
@@ -1,23 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useDebounce } from "../hooks/useDebounce";
+import InputCategory from "../components/InputCategory";
 
 function HomePages() {
-  const [state, setState] = useState();
+  const [product, setProduct] = useState();
+  const [_payloadSearch, setPayloadSearch] = useState({ query: "", category: "" });
+  const payloadSearch = useDebounce(_payloadSearch, 1000);
 
   useEffect(() => {
-    fetch("https://dummyjson.com/products").then((res) => {
-      res.json().then(setState);
-      console.log(res);
-    });
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then(setProduct);
   }, []);
+
+  useEffect(() => {
+    console.log(payloadSearch);
+    if (payloadSearch)
+      fetch(`https://dummyjson.com/products/search?q=${payloadSearch.query}`)
+        .then((res) => res.json())
+        .then(setProduct);
+
+    if (payloadSearch.category)
+      fetch(`https://dummyjson.com/products/category/${payloadSearch.category}`)
+        .then((res) => res.json())
+        .then(setProduct);
+  }, [payloadSearch]);
+
+  const handleQuerySearch = (e) => {
+    // e.preventDefault();
+
+    const queryInputSearch = e.target.value;
+    setPayloadSearch({ category: "", query: queryInputSearch });
+  };
+
+  const handleCategory = (e) => {
+    const category = e.target.value;
+    setPayloadSearch({ query: "", category });
+  };
 
   return (
     <div>
       <h1>Google E-Commerce</h1>
       <p>Our Products</p>
-      {state ? (
+
+      <div>
+        <label htmlFor="queryInputSearch">Search Products</label>
+        <input
+          id="queryInputSearch"
+          type="text"
+          name="queryInputSearch"
+          value={_payloadSearch.query}
+          onChange={handleQuerySearch}
+        />
+
+        <div>
+          <InputCategory value={_payloadSearch.category} handleCategory={handleCategory} />
+        </div>
+        {payloadSearch.query && <p>Searching for : {payloadSearch.query}</p>}
+        {payloadSearch.category && <p>Searching for category : {payloadSearch.category}</p>}
+      </div>
+
+      {product ? (
         <div className="products">
-          {state.products.map((item) => (
+          {product.products.map((item) => (
             <div className="products_item" key={item.id}>
               <h3>{item.title}</h3>
               <h4>({item.brand})</h4>
